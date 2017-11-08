936812da7d70c9b1979ecec2c6f487c11c3aff49
1178981326@qq.com <wo951127>
2017-11-08 15:21:55 +0800

    all models

--- cms/static/datas/nav.js
+++ cms/static/datas/nav.js
@@ -1,93 +1,69 @@
 var navs = [{
-	"title": "基本元素",
-	"icon": "fa-cubes",
-	"spread": true,
-	"children": [{
-		"title": "按钮",
-		"icon": "&#xe641;",
-		"href": "button.html"
-	}, {
-		"title": "表单",
-		"icon": "&#xe63c;",
-		"href": "form.html"
-	}, {
-		"title": "表格",
-		"icon": "&#xe63c;",
-		"href": "table.html"
-	}, {
-		"title": "导航",
-		"icon": "&#xe609;",
-		"href": "nav.html"
-	}, {
-		"title": "辅助性元素",
-		"icon": "&#xe60c;",
-		"href": "auxiliar.html"
-	}]
+    "title": "用户管理",
+    "spread": true,
+    "children": [{
+        "title": "查看用户",
+        "href": "button.html"
+    }, {
+        "title": "权限分配",
+        "href": "form.html"
+    }, {
+        "title": "开启/关闭报警模块",
+        "href": "form.html"
+    }]
 }, {
-	"title": "组件",
-	"icon": "fa-cogs",
-	"spread": false,
-	"children": [{
-		"title": "BTable",
-		"icon": "fa-table",
-		"href": "btable.html"
-	}, {
-		"title": "Navbar组件",
-		"icon": "fa-navicon",
-		"href": "navbar.html"
-	}, {
-		"title": "Tab组件",
-		"icon": "&#xe62a;",
-		"href": "tab.html"
-	}, {
-		"title": "Laytpl+Laypage",
-		"icon": "&#xe628;",
-		"href": "paging.html"
-	}]
+    "title": "设备管理",
+    "spread": false,
+    "children": [{
+        "title": "设备管理",
+        "href": "btable.html"
+    }]
+},  {
+    "title": "日志",
+    "href": "",
+    "spread": false,
+    "children": [{
+        "title": "登陆日志",
+        "href": "https://www.github.com/"
+    }, {
+        "title": "操作日志",
+        "href": "http://www.qq.com/"
+    }, {
+        "title": "报警日志",
+        "href": "http://fly.layui.com/"
+    }]
 }, {
-	"title": "第三方组件",
-	"icon": "&#x1002;",
-	"spread": false,
-	"children": [{
-		"title": "iCheck组件",
-		"icon": "fa-check-square-o",
-		"href": "icheck.html"
-	}]
+    "title": "生产管理",
+    "href": "https://www.baidu.com",
+    "spread": false,
+    "children": [{
+        "title": "ERP系统",
+        "href": "cop.html"
+    }, {
+        "title": "设备管理",
+        "href": "form.html"
+    }, {
+        "title": "质量管理",
+        "href": "form.html"
+    }, {
+        "title": "用户图纸",
+        "href": "form.html"
+    }, {
+        "title": "预留",
+        "href": "form.html"
+    }]
 }, {
-	"title": "地址本",
-	"icon": "fa-address-book",
-	"href": "",
-	"spread": false,
-	"children": [{
-		"title": "Github",
-		"icon": "fa-github",
-		"href": "https://www.github.com/"
-	}, {
-		"title": "QQ",
-		"icon": "fa-qq",
-		"href": "http://www.qq.com/"
-	}, {
-		"title": "Fly社区",
-		"icon": "&#xe609;",
-		"href": "http://fly.layui.com/"
-	}, {
-		"title": "新浪微博",
-		"icon": "fa-weibo",
-		"href": "http://weibo.com/"
-	}]
-}, {
-	"title": "这是一级导航",
-	"icon": "fa-stop-circle",
-	"href": "https://www.baidu.com",
-	"spread": false
-}, {
-	"title": "其他",
-	"icon": "fa-stop-circle",
-	"href": "#",
-	"spread": false,
-	"children": [{
-		"title": "子窗体中打开选项卡",
-		"icon": "fa-github",
-		"href": "cop.html"
-	}]
+    "title": "安保系统",
+    "href": "#",
+    "spread": false,
+    "children": [{
+        "title": "门禁管理",
+        "href": "cop.html"
+    }, {
+        "title": "视频监控",
+        "href": "cop.html"
+    }, {
+        "title": "环境监控",
+        "href": "cop.html"
+    }]
 }];
--- cms/static/js/index.js
+++ cms/static/js/index.js
@@ -65,7 +65,7 @@ layui.config({
         cached: true,
         data: navs
 		/*cached:true,
-		url: 'datas/nav.json'*/
+		url: 'datas/nav123.json'*/
     });
     //渲染navbar
     navbar.render();
@@ -135,16 +135,16 @@ layui.config({
     });
 
     //锁屏
-    $(document).on('keydown', function () {
-        var e = window.event;
-        if (e.keyCode === 76 && e.altKey) {
-            //alert("你按下了alt+l");
-            lock($, layer);
-        }
-    });
-    $('#lock').on('click', function () {
-        lock($, layer);
-    });
+    // $(document).on('keydown', function () {
+    //     var e = window.event;
+    //     if (e.keyCode === 76 && e.altKey) {
+    //         //alert("你按下了alt+l");
+    //         lock($, layer);
+    //     }
+    // });
+    // $('#lock').on('click', function () {
+    //     lock($, layer);
+    // });
 
     //手机设备的简单适配
     var treeMobile = $('.site-tree-mobile'),
@@ -158,82 +158,82 @@ layui.config({
 });
 
 var isShowLock = false;
-function lock($, layer) {
-    if (isShowLock)
-        return;
-    //自定页
-    layer.open({
-        title: false,
-        type: 1,
-        closeBtn: 0,
-        anim: 6,
-        content: $('#lock-temp').html(),
-        shade: [0.9, '#393D49'],
-        success: function (layero, lockIndex) {
-            isShowLock = true;
-            //给显示用户名赋值
-            //layero.find('div#lockUserName').text('admin');
-            //layero.find('input[name=username]').val('admin');
-            layero.find('input[name=password]').on('focus', function () {
-                var $this = $(this);
-                if ($this.val() === '输入密码解锁..') {
-                    $this.val('').attr('type', 'password');
-                }
-            })
-                .on('blur', function () {
-                    var $this = $(this);
-                    if ($this.val() === '' || $this.length === 0) {
-                        $this.attr('type', 'text').val('输入密码解锁..');
-                    }
-                });
-            //在此处可以写一个请求到服务端删除相关身份认证，因为考虑到如果浏览器被强制刷新的时候，身份验证还存在的情况			
-            //do something...
-            //e.g. 
-
-            $.getJSON('/Account/Logout', null, function (res) {
-                if (!res.rel) {
-                    layer.msg(res.msg);
-                }
-            }, 'json');
-
-            //绑定解锁按钮的点击事件
-            layero.find('button#unlock').on('click', function () {
-                var $lockBox = $('div#lock-box');
-
-                var userName = $lockBox.find('input[name=username]').val();
-                var pwd = $lockBox.find('input[name=password]').val();
-                if (pwd === '输入密码解锁..' || pwd.length === 0) {
-                    layer.msg('请输入密码..', {
-                        icon: 2,
-                        time: 1000
-                    });
-                    return;
-                }
-                unlock(userName, pwd);
-            });
-			/**
-			 * 解锁操作方法
-			 * @param {String} 用户名
-			 * @param {String} 密码
-			 */
-            var unlock = function (un, pwd) {
-                console.log(un, pwd);
-                //这里可以使用ajax方法解锁
-                $.post('/Account/UnLock', { userName: un, password: pwd }, function (res) {
-                    //验证成功
-                    if (res.rel) {
-                        //关闭锁屏层
-                        layer.close(lockIndex);
-                        isShowLock = false;
-                    } else {
-                        layer.msg(res.msg, { icon: 2, time: 1000 });
-                    }
-                }, 'json');
-                //isShowLock = false;
-                //演示：默认输入密码都算成功
-                //关闭锁屏层
-                //layer.close(lockIndex);
-            };
-        }
-    });
-};
+// function lock($, layer) {
+//     if (isShowLock)
+//         return;
+//     //自定页
+//     layer.open({
+//         title: false,
+//         type: 1,
+//         closeBtn: 0,
+//         anim: 6,
+//         content: $('#lock-temp').html(),
+//         shade: [0.9, '#393D49'],
+//         success: function (layero, lockIndex) {
+//             isShowLock = true;
+//             //给显示用户名赋值
+//             //layero.find('div#lockUserName').text('admin');
+//             //layero.find('input[name=username]').val('admin');
+//             layero.find('input[name=password]').on('focus', function () {
+//                 var $this = $(this);
+//                 if ($this.val() === '输入密码解锁..') {
+//                     $this.val('').attr('type', 'password');
+//                 }
+//             })
+//                 .on('blur', function () {
+//                     var $this = $(this);
+//                     if ($this.val() === '' || $this.length === 0) {
+//                         $this.attr('type', 'text').val('输入密码解锁..');
+//                     }
+//                 });
+//             //在此处可以写一个请求到服务端删除相关身份认证，因为考虑到如果浏览器被强制刷新的时候，身份验证还存在的情况
+//             //do something...
+//             //e.g.
+//
+//             $.getJSON('/Account/Logout', null, function (res) {
+//                 if (!res.rel) {
+//                     layer.msg(res.msg);
+//                 }
+//             }, 'json');
+//
+//             //绑定解锁按钮的点击事件
+//             // layero.find('button#unlock').on('click', function () {
+//             //     var $lockBox = $('div#lock-box');
+//             //
+//             //     var userName = $lockBox.find('input[name=username]').val();
+//             //     var pwd = $lockBox.find('input[name=password]').val();
+//             //     if (pwd === '输入密码解锁..' || pwd.length === 0) {
+//             //         layer.msg('请输入密码..', {
+//             //             icon: 2,
+//             //             time: 1000
+//             //         });
+//             //         return;
+//             //     }
+//             //     unlock(userName, pwd);
+//             // });
+// 			/**
+// 			 * 解锁操作方法
+// 			 * @param {String} 用户名
+// 			 * @param {String} 密码
+// 			 */
+//             // var unlock = function (un, pwd) {
+//             //     console.log(un, pwd);
+//             //     //这里可以使用ajax方法解锁
+//             //     $.post('/Account/UnLock', { userName: un, password: pwd }, function (res) {
+//             //         //验证成功
+//             //         if (res.rel) {
+//             //             //关闭锁屏层
+//             //             layer.close(lockIndex);
+//             //             isShowLock = false;
+//             //         } else {
+//             //             layer.msg(res.msg, { icon: 2, time: 1000 });
+//             //         }
+//             //     }, 'json');
+//             //     //isShowLock = false;
+//             //     //演示：默认输入密码都算成功
+//             //     //关闭锁屏层
+//             //     //layer.close(lockIndex);
+//             // };
+//         }
+//     });
+// };
